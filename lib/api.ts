4faf4e59d848f2a0bed0172b32420250bdc34adb
SarveshBhoite <rajb81008@gmail.com>
2025-12-01 14:53:08 +0530

    backend deployed

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://192.168.1.11:5000",
+  baseURL: "https://todoapp-8clx.onrender.com",
   timeout:5000, // 🔥 IMPORTANT update below
 });
 
